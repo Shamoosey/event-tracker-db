b78e6a961f20f96dbfd97cfc72f8d8addadcf620
sosler <shamuso23@gmail.com>
2026-01-17 18:57:11 -0600

    Added cascade deletes

---
+++ prisma/migrations/20260118004813_add_cascade_deletes/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "EventAttendee" DROP CONSTRAINT "EventAttendee_eventId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "EventAttendee" DROP CONSTRAINT "EventAttendee_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EventAttendee" ADD CONSTRAINT "EventAttendee_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EventAttendee" ADD CONSTRAINT "EventAttendee_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
